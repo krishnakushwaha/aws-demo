a42e7f26cbbdb838605d760b3f3ac998a9643c37
krishnakushwaha <krishnakushwaha44@gmail.com>
2025-06-24 22:53:06 +0530

    Update start_container.shv1

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,6 +4,6 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull krishnakushwaha/simple-python-flask-app
 
-# Run the Docker image as a container22
+# Run the Docker image as a container2244
 docker run -d -p 5000:5000 krishnakushwaha/simple-python-flask-app
 
