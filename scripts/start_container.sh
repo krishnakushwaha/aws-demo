9e733bedeed7be6c91c5b18044d44ffbda82d733
krishnakushwaha <krishnakushwaha44@gmail.com>
2025-07-13 18:45:23 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-docker pull krishnakushwaha/simple-python-flask-app
+# Pull the Docker image
+sudo docker pull krishnakushwaha/simple-python-flask-app
 
-# Run the Docker image as a container2244
-docker run -d -p 5000:5000 krishnakushwaha/simple-python-flask-app
-
+# Run the Docker container
+sudo docker run -d -p 5000:5000 krishnakushwaha/simple-python-flask-app
