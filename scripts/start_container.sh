0d459fe2ef6bc8eaba9727ed9bc5f218d8ad0ada
krishnakushwaha <krishnakushwaha44@gmail.com>
2025-07-13 20:28:05 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-# Stop and remove any existing container
-sudo docker rm -f container2244 || true
+# Stop & remove existing container if it exists
+sudo docker rm -f flaskapp || true
 
-# Remove the local image so it fetches fresh one from Docker Hub
+# Remove existing image (optional)
 sudo docker rmi krishnakushwaha/simple-python-flask-app:latest || true
 
-# Pull the latest version from Docker Hub
+# Pull latest image
 sudo docker pull krishnakushwaha/simple-python-flask-app:latest
 
-# Run the container
-sudo docker run -d --name container2244 -p 5000:5000 krishnakushwaha/simple-python-flask-app:latest
-
+# Run container
+sudo docker run -d --name flaskapp -p 5000:5000 krishnakushwaha/simple-python-flask-app:latest
